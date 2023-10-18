e2f677c3fd124c592703eb256f0ebbed108ea7c0
Nelson <nealson@gmail.com>
2023-10-18 23:18:54 +0800

    Generate a syntax tree by EBNF and produce x86-64 pop,push instructions for arithmetic operations.

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -10,7 +10,42 @@ typedef enum{
     TK_EOF,      //end symbol
 } TokenKind;
 
+typedef enum{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // number
+} NodeKind;
+
+typedef struct Node Node;
 typedef struct Token Token;
+
+struct Node{
+    NodeKind kind; // kind of node
+    Node *l;
+    Node *r;
+    int val;
+};
+
+Node *new_node(NodeKind kind,Node *l,Node *r){
+    Node *cur = calloc(1, sizeof(Node));
+    cur->kind = kind;
+    cur->l = l;
+    cur->r = r;
+    return cur;
+}
+
+Node *new_node_num(int val){
+    Node *cur = calloc(1,sizeof(Node));
+    cur->kind = ND_NUM;
+    cur->val = val;
+    return cur;
+}
+
+Node *expr();
+Node *mul();
+Node *term();
 
 Token *token;
 char *user_input;
@@ -61,7 +96,7 @@ Token *__tokenize(){
             p++;
             continue;;
         }
-        if(*p=='+' || *p=='-'){
+        if(strchr("+-*/()", *p)){
             cur = __new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -98,6 +133,70 @@ bool consume(char op){
     return true;
 }
 
+/*
+ expr = mul ('+' mul | '-' mul) *
+ mul = term ('*' term | '/' term) *
+ term = num | '(' expr ')'
+*/
+
+Node *expr(){
+    Node *cur = mul();
+    for(;;){
+        if(consume('+')) cur = new_node(ND_ADD, cur, mul());
+        else if(consume('-')) cur = new_node(ND_SUB, cur, mul());
+        else return cur;
+    }
+}
+
+Node *mul(){
+    Node *node = term();
+    for(;;){
+        if(consume('*')) node = new_node(ND_MUL, node, term());
+        else if(consume('/')) node = new_node(ND_DIV, node, term());
+        else return node;
+    }
+}
+
+Node *term(){
+    if(consume('(')){
+        Node *cur = expr();
+        expect(')');
+        return cur;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("  push %d\n",node->val);
+        return;
+    }
+
+    gen(node->l);
+    gen(node->r);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch(node->kind){
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 
 int main(int argc,char **argv){
     if(argc != 2){
@@ -107,22 +206,15 @@ int main(int argc,char **argv){
 
     user_input = argv[1];
     token = __tokenize();
+    Node *cur_node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n",expect_number());
 
-    while(!at_eof(token)){
-        if(consume('+'))
-        {
-            printf("  add rax, %d\n",expect_number());
-            continue;
-        }
-        expect('-');
-        printf("  sub rax, %d\n",expect_number());
-    }
+    gen(cur_node);
 
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
